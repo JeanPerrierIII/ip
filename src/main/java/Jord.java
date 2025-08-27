90fb793408d4c73f114b00c1133336cded7817f2
szeanh <szeanh.goh@gmail.com>
2025-08-27 12:36:51 +0800

    Renamed chatbot to Jord, changed greeting message.

---
+++ src/main/java/Jord.java
@@ -0,0 +1,5 @@
+public class Jord {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Jord\n I cannot do anything as of now, so bye, hope to see you soon!");
+    }
+}
