9bca6f7fe979e62e53574854d19de8425250475c
szeanh <szeanh.goh@gmail.com>
2025-08-28 10:13:58 +0800

    Implemented list ability, tasks may be added and listed.

--- src/main/java/Jord.java
+++ src/main/java/Jord.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class Jord {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String line;
         Scanner in = new Scanner(System.in);
         System.out.println("    Hello! I'm Jord\n    What can I do for you?");
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println("    " + line);
+            if (line.equals("list")) {
+                // print consecutively, list of tasks
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i+1) + ". " + tasks[i]);
+                }
+            }
+            else {
+                // add to list, increment taskCount
+                tasks[taskCount] = line;
+                System.out.println("    added: " + line);
+                taskCount++;
+            }
             line = in.nextLine();
         }
-        System.out.println("    Bye! See you again!");
+
+        System.out.println("    Bye, see you again!");
     }
 }
