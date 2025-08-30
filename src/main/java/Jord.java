4076b6fb2486c8b6550ea870178ea847dd5143b9
szeanh <szeanh.goh@gmail.com>
2025-08-30 23:39:37 +0800

    Refactor code to comply with coding standards

--- src/main/java/Jord.java
+++ src/main/java/Jord.java
@@ -2,43 +2,66 @@ import java.util.Scanner;
 
 public class Jord {
 
-    public static void printTasks(Task[] tasks) {
+    private static final int TASK_LIMIT = 100;
+    private static Task[] TASKS = new Task[TASK_LIMIT];
+    private static int TASK_COUNT = 0;
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void printTasks() {
         int i = 0;
-        while (tasks[i] != null) {
+        while (TASKS[i] != null) {
             System.out.print("    " + (i+1) + ". ");
-            tasks[i].printStatus();
+            TASKS[i].printStatus();
             i++;
         }
     }
 
-    public static void main(String[] args) {
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
-        String line;
-        Scanner in = new Scanner(System.in);
-        System.out.println("    Hello! I'm Jord\n    What can I do for you?");
-        line = in.nextLine();
-        while (!line.equals("bye")) {
-            if (line.equals("list")) {
-                printTasks(tasks);
-            }
-            else if (line.contains("mark")) {
-                String[] words = line.split(" ");
-                int index = Integer.parseInt(words[1]) - 1;
-                tasks[index].setMarked(!words[0].contains("un"));
-                System.out.println(words[0].contains("un") ? "    The following task hae been marked incomplete" : "    The following task has been marked complete");
-                System.out.print("    ");
-                tasks[index].printStatus();
-            }
-            else {
-                // add to list, increment taskCount
-                tasks[taskCount] = new Task(line);
-                System.out.println("    added: " + line);
-                taskCount++;
-            }
-            line = in.nextLine();
-        }
-
-        System.out.println("    Bye, see you again!");
+    public static void taskMarker(String input) {
+        String[] words = input.split(" ");
+        int index = Integer.parseInt(words[1]) - 1;
+        TASKS[index].setMarked(!words[0].contains("un"));
+        System.out.println(words[0].contains("un") ? "    The following task has been marked incomplete"
+                : "    The following task has been marked complete");
+        System.out.print("    ");
+        TASKS[index].printStatus();
     }
+
+    public static void taskAdder(String input) {
+        TASKS[TASK_COUNT] = new Task(input);
+        System.out.println("    added: " + input);
+        TASK_COUNT++;
+    }
+
+    public static String getUserInput() {
+        String line = SCANNER.nextLine();
+        return line;
+    }
+
+    public static void exitJord() {
+        System.out.println("    Bye, see you again!");
+        System.exit(0);
+    }
+
+    private static void processInput(String input) {
+        if (input.equals("list")) {
+            printTasks();
+        } else if (input.contains("mark")) {
+            taskMarker(input);
+        } else if (input.equals("bye")) {
+            exitJord();
+        } else {
+            taskAdder(input);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("    Hello! I'm Jord\n    What can I do for you?");
+        while (true) {
+            String userInput = getUserInput();
+            processInput(userInput);
+        }
+    }
+
+
 }
