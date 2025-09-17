4a82efcfb4e11504619892b1181bb97de290792a
szeanh <szeanh.goh@gmail.com>
2025-09-18 02:31:44 +0800

    Add save functionality

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -2,11 +2,23 @@ package tasks;
 
 public class Deadline extends Task {
 
-    protected String by;
+    protected String by = null;
 
+    public Deadline() {}
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
+    }
+
+    @Override
+    public void load(String[] splitInput) {
+        super.load(splitInput);
+        by = splitInput[3];
+    }
+
+    @Override
+    public String save() {
+        return "D" + super.saveHelper() + ";" + by;
     }
 
     @Override
--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,11 +1,27 @@
 package tasks;
 
-public class Task {
-    private final String description;
+public class Task implements TaskIO {
+    private String description = null;
     private boolean isMarked = false;
+
+    public Task() {}
 
     public Task(String description) {
         this.description = description;
+    }
+
+    public void load(String[] splitInput) {
+        isMarked = (Integer.parseInt(splitInput[1]) == 1);
+        description = splitInput[2];
+    }
+
+    protected String saveHelper () {
+        return (";" + (isMarked ? "1" : "0") + ";" + description);
+    }
+
+    public String save(){
+        // T is reserved for todo
+        return "X" + saveHelper();
     }
 
     public void setMarked(boolean marked) {
